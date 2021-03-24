996beb510296fd8603e9a6e8ba89cce1cd938cda
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-03-24 12:51:31 +0100

    added admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cat
+from .models import User
+from .models import UserCatOwner
+from .models import Prey
+from .models import Hunting
+from .models import HuntingDetails
 
-# Register your models here.
+admin.site.register(Cat)
+admin.site.register(User)
+admin.site.register(UserCatOwner)
+admin.site.register(Prey)
+admin.site.register(Hunting)
+admin.site.register(HuntingDetails)
