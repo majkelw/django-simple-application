aaac34d1257b5a54a06de9e8231fb3d3524e04ed
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-03-24 18:05:19 +0100

    small fix, added brackets

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
     email = models.CharField(max_length=40)
-    cats_number = models.IntegerField
+    cats_number = models.IntegerField(default=0)
 
     def __str__(self):
         return "%s %s" % (self.name, self.email)
