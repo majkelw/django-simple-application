c0bc179ed2b5f70e9dea69fd0d4bc18fe60dfad4
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-03-24 21:48:09 +0100

    small fix, changed viewsets.ModelViewSet to viewsets.ReadOnlyViewSet

--- app/views.py
+++ app/views.py
@@ -3,16 +3,16 @@ from rest_framework import viewsets
 from .serializer import UserSerializer, UserCatOwnerSerializer, CatSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserCatOwnerViewSet(viewsets.ModelViewSet):
+class UserCatOwnerViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = UserCatOwner.objects.all()
     serializer_class = UserCatOwnerSerializer
 
 
-class CatViewSet(viewsets.ModelViewSet):
+class CatViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
